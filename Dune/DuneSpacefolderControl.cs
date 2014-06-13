9a1838b5717179ceda8fa636f315fef7369b7111
Yilmas <github@nt-jensen.dk>
2014-06-13 23:58:32 +0200

    First step towards implementing Spacefolders.

---
+++ Dune/DuneSpacefolderControl.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Dune
+{
+    public class DuneSpacefolderControl : ControlModule
+    {
+        public DuneSpacefolderControl(DuneCore core)
+            : base(core)
+        {
+            runModuleInScenes.Add(GameScenes.FLIGHT);
+        }
+
+        private DuneSpacefolderModule spacefolderModule;
+
+        public bool settingsRetrieved { get; private set; }
+        public string engineName { get; private set; }
+        public double engineEfficiency { get; private set; }
+        public double engineFailure { get; private set; }
+
+        public override void OnUpdate()
+        {
+            if (!FlightGlobals.ActiveVessel.IsNull())
+            {
+                if (FlightGlobals.ActiveVessel.GetModules<DuneSpacefolderModule>().Count > 0)
+                {
+                    spacefolderModule = (DuneSpacefolderModule)FlightGlobals.ActiveVessel.GetMasterObject<DuneSpacefolderModule>();
+                }
+                else
+                {
+                    spacefolderModule = null;
+                }
+            }
+
+            if (!spacefolderModule.IsNull())
+            {
+                settingsRetrieved = true;
+                engineName = spacefolderModule.engineName;
+                engineEfficiency = spacefolderModule.engineEfficiency;
+                engineFailure = spacefolderModule.engineFailure;
+            }
+            else
+            {
+                settingsRetrieved = false;
+            }
+        }
+    }
+}
---
+++ Dune/DuneSpacefolderModule.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+namespace Dune
+{
+    public class DuneSpacefolderModule : PartModule
+    {
+        public string engineName { get; private set; }
+        public double engineEfficiency { get; private set; }
+        public double engineFailure { get; private set; }
+
+        public ConfigNode configModule;
+
+        public override void OnStart(PartModule.StartState state)
+        {
+            if (state != StartState.Editor && state != StartState.None)
+            {
+                if (!FlightGlobals.ActiveVessel.IsNull())
+                {
+                    engineName = part.name;
+                    engineEfficiency = double.Parse(configModule.GetValue("efficiency"));
+                    engineFailure = double.Parse(configModule.GetValue("failure"));
+                }
+            }
+        }
+
+        public override void OnLoad(ConfigNode node)
+        {
+            if (configModule == null)
+            {
+                configModule = new ConfigNode();
+                node.CopyTo(configModule);
+            }
+            base.OnLoad(node);
+        }
+
+        public override void OnSave(ConfigNode node)
+        {
+            if (node != null) base.OnSave(node);
+            else base.OnSave(configModule);
+        }
+    }
+}
